e61028a56628bc291a4d4dbb08117f2f73d8e116
matheuzeba <tavaresviana82@gmail.com>
2023-08-26 18:59:05 -0300

    criado funcionalidade de criarTarefa

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,22 +1,23 @@
 const todo = require('../models/todo')
 
 const listarTodasAsTarefas = async (req,res) => {
-    const encontrar = await todo.find({})
-    res.status(200).json(encontrar)
+    const tarefa = await todo.find({})
+    res.status(200).json(tarefa)
 }
 
 const listarTarefaId = async (req, res) => {
     const {id} = req.params
-    const encontrar = await todo.findById(id)
-    res.status(200).json(encontrar)
+    const tarefa = await todo.findById(id)
+    res.status(200).json(tarefa)
 }
 
 const atualizarTarefaId = (req, res) => {
     res.json ('atualizar uma tarefa por id')
 }
 
-const criarTarefa = (req, res) => {
-    res.json ('criar uma tarefa')
+const criarTarefa = async (req, res) => {
+    const tarefa = await todo.create(req.body)
+    res.status(200).json(tarefa)
 }
 
 const deletarTarefa = (req, res) => {
