ab7374f9e32e905c6172eedcffc7392c98fbbc3f
matheuzeba <tavaresviana82@gmail.com>
2023-08-26 19:42:50 -0300

    adicionado wrapper a todas as rotas

--- src/controllers/todo.js
+++ src/controllers/todo.js
@@ -1,35 +1,51 @@
 const todo = require('../models/todo');
+const asyncWrapper = require('../middlewares/async');
 
-const listarTodasAsTarefas = async (req,res) => {
-    const tarefa = await todo.find({})
-    res.status(200).json(tarefa)
-}
+const listarTodasAsTarefas = asyncWrapper(async (req,res) => {
+    const tarefa = await todo.find({});
+    res.status(200).json(tarefa);
+})
 
-const listarTarefaId = async (req, res) => {
-    const {id} = req.params
-    const tarefa = await todo.findById(id)
-    res.status(200).json(tarefa)
-}
+const listarTarefaId = asyncWrapper(async (req, res) => {
+    const {id} = req.params;
+    const tarefa = await todo.findById(id);
+    
+    if(!tarefa) {
+        return res.status(404).json({mensagem: "este id nao existe"});
+    }
 
-const atualizarTarefaId = async (req, res) => {
-    const {id} = req.params
+    res.status(200).json(tarefa);
+})
+
+const atualizarTarefaId = asyncWrapper(async (req, res) => {
+    const {id} = req.params;
     const tarefa = await todo.findByIdAndUpdate(id, req.body, {
         new:true,
         runValidators: true
-    })
-    res.status(200).json(tarefa)
-}
+    });
+    
+    if(!tarefa) {
+        return res.status(404).json({mensagem: "este id nao existe"});
+    }
 
-const criarTarefa = async (req, res) => {
-    const tarefa = await todo.create(req.body)
-    res.status(200).json(tarefa)
-}
+    res.status(200).json(tarefa);
+})
 
-const deletarTarefa = async (req, res) => {
-    const {id} = req.params
-    const tarefa = await todo.findByIdAndDelete(id)
-    res.status(200).json(tarefa)
-}
+const criarTarefa = asyncWrapper(async (req, res) => {
+    const tarefa = await todo.create(req.body);
+    res.status(200).json(tarefa);
+})
+
+const deletarTarefa = asyncWrapper(async (req, res) => {
+    const {id} = req.params;
+    const tarefa = await todo.findByIdAndDelete(id);
+
+    if(!tarefa) {
+        return res.status(404).json({mensagem: "este id nao existe"});
+    }
+
+    res.status(200).json(tarefa);
+})
 
 module.exports = {
     listarTodasAsTarefas,
--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,8 +1,10 @@
-const asyncWrapper = async (funcao) => {
-    try{
-        await funcao(req, res, next)
-    } catch(error) {
-        console.log(error)
+const asyncWrapper = (funcao) => {
+    return async(req, res, next) => {
+        try{
+            await funcao(req, res, next)
+        } catch(error) {
+            res.status(500).json({message: error})
+        }
     }
 }
 
